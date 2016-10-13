80d31d3299475b825501b7f3bf012853c54549ce
Daniel Kostro <kostro.d@gmail.com>
2016-10-13 10:46:54 +0200

    export Serie

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var Equilibrium = require('./core/Equilibrium');
-var Helper = require('./helpers/Helper');
+const Equilibrium = require('./core/Equilibrium');
+const Helper = require('./helpers/Helper');
+const Serie = require('./helpers/Serie');
 
 Equilibrium.Helper = Helper;
+Equilibrium.Serie = Serie;
 
 module.exports = Equilibrium;
