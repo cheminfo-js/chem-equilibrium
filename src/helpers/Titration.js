919e9ad27f58071c5399fe5bd288f2c856d2a332
Daniel Kostro <kostro.d@gmail.com>
2016-10-13 16:44:43 +0200

    Serie helper: add titration

--- src/helpers/Titration.js
+++ src/helpers/Titration.js
@@ -1,5 +1,7 @@
 'use strict';
 
+'use strict';
+
 const defaultOptions = {
     chunks: 200,
     log: false,
@@ -14,72 +16,59 @@ class Serie {
     }
 
     getTitration(options) {
-        options = Object.assign({}, defaultOptions, options);
-        // We don't want to change the original helper
-        var helper = this.helper.clone();
-        helper.resetSpecies();
-        // Some options are meant for the helper
-        // e.g. tolerance, solidTolerance, maxIterations
-        helper.setOptions(options);
-
-
-        var solVolume = options.solution.volume;
-        var solConc = options.solution.concentration;
+        var form = get('form');
+        var solComp = form.solution.type;
+        var solVolume = form.solution.volume;
+        var solConc = form.solution.concentration;
         var solQty = solConc * solVolume;
 
-        var titrConc = options.titrationSolution.concentration;
+        var titrConc = form.titrationSolution.concentration;
         var titrVolStart = 0;
-        var titrVolStop = options.titrationSolution.volume;
+        var titrVolStop = form.titrationSolution.volume;
 
         var errorCount = 0;
 
         var vols = [];
         var ph = [];
-        var solutions = [];
-        var chunks = options.chunks;
+        var chunks = 500;
         var sol;
+        var acidModel = new Factory();
 
-        helper.addSpecie(options.solution.type);
-        helper.addSpecie(options.titrationSolution.type);
-
-        for (var i = 0; i <= chunks; i++) {
-            var vol = titrVolStart + (titrVolStop - titrVolStart) * (i / chunks);
+        for(var i=0; i<=chunks; i++) {
+            var vol = titrVolStart + (titrVolStop - titrVolStart) * (i/chunks)
             var totalVol = vol + solVolume;
             var titrQty = vol * titrConc;
-            helper.setTotal(options.titrationSolution.type, titrQty);
-            helper.setTotal(options.solution.type, solQty);
-            helper.setOptions({volume: totalVol});
-            var eq = helper.getEquilibrium();
+            acidModel.reset();
+            acidModel.addSpecie(form.titrationSolution.type, titrQty);
+            acidModel.addSpecie(form.solution.type, solQty);
+            acidModel.setOptions({volume: totalVol});
+            var eq = acidModel.getEquilibrium();
 
-            if (!sol) {
+            if(!sol) {
                 sol = eq.solveRobust();
             } else {
                 eq.setInitial(sol);
                 sol = eq.solve();
             }
 
-            if (sol) {
+            if(sol) {
                 ph.push(-Math.log10(sol['H+']));
-                solutions.push(sol);
-                vols.push(vol);
             } else {
+                ph.push(NaN);
                 errorCount++;
             }
 
+            vols.push(vol);
         }
 
         var xy = [];
-        for (var i = 0; i < ph.length; i++) {
+        for(var i=0; i<ph.length; i++) {
+            if(isNaN(ph[i])) continue;
             xy.push(vols[i], ph[i]);
         }
+        API.createData('xy', xy);
+        API.createData('errorCount', errorCount);
 
-        var species = solutions[0] ? Object.keys(solutions[0]) : [];
-
-        return {
-            xy, errorCount, solutions, species,
-            volumes: vols,
-            equations: helper.getEquations({filtered: true})
-        }
     }
 
     getSolutions(options) {
@@ -117,27 +106,27 @@ class Serie {
         //     to = Number(options.to);
         // }
 
-        for (var i = 0; i <= chunks; i++) {
+        for(var i=0; i<=chunks; i++) {
             var val = options.from + (to - from) * i / chunks;
 
-            if (log) {
+            if(log) {
                 var realVal = Math.pow(10, -val);
             } else {
                 realVal = val;
             }
-            if (options.isFixed) {
+            if(options.isFixed) {
                 helper.setAtEquilibrium(varying, realVal);
             } else {
                 helper.setTotal(varying, realVal);
             }
             var eq = helper.getEquilibrium();
-            if (sol) {
+            if(sol) {
                 eq.setInitial(sol);
                 sol = eq.solve();
             } else {
                 sol = eq.solveRobust();
             }
-            if (sol) {
+            if(sol) {
                 x.push(val);
                 solutions.push(sol);
             } else {
@@ -152,11 +141,11 @@ class Serie {
 }
 
 function checkOptions(options) {
-    if (options.from >= options.to) {
+    if(options.from >= options.to) {
         throw new Error('Invalid arguments: property "to" should be larger than "from"');
     }
 
-    if (!options.varying) {
+    if(!options.varying) {
         throw new Error('Invalid arguments: property "varying" is not defined');
     }
 }
